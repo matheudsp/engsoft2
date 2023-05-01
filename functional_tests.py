9e4759f2396efcce7bf8634a45e3d3617537a885
matheudsp <mdsp.personal@gmail.com>
2023-05-01 16:35:36 -0300

    finished cap3

--- functional_tests.py
+++ functional_tests.py
@@ -1,18 +1,31 @@
-# Required libraries
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
 # Define Brave path
 brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 options = webdriver.ChromeOptions()
 options.binary_location = brave_path
 
+class NewVisitorTest(unittest.TestCase):  
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    def setUp(self):  
+        self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-driver.get('http://127.0.0.1:8000/')
+    def tearDown(self):  
+        self.browser.quit()
 
-# assert 'Django' in driver.title
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)  
+        self.fail('Finish the test!')  
 
+        # She is invited to enter a to-do item straight away
+        # [...rest of comments as before]
+
+if __name__ == '__main__':  
+    unittest.main()
